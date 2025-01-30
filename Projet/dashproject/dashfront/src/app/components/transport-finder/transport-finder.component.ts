9d909ccb2299a2a13119af26fc9dca255776742f
Poirier Victor <victor.poirier.etu@univ-lemans.fr>
2025-01-30 16:35:34 +0100

    Ajout initial du projet

---
+++ Projet/dashproject/dashfront/src/app/components/transport-finder/transport-finder.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transport-finder',
+  imports: [],
+  templateUrl: './transport-finder.component.html',
+  styleUrl: './transport-finder.component.css'
+})
+export class TransportFinderComponent {
+
+}
